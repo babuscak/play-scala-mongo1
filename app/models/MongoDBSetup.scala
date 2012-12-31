27e6e782f2c22e888ad267365561cb752a46a9ca
Roman Babuscak <roman.babuscak@garwan.sk>
2012-12-31 02:14:09 +0100

    fix

--- app/models/MongoDBSetup.scala
+++ app/models/MongoDBSetup.scala
@@ -2,7 +2,7 @@ package models
 import com.mongodb.casbah.MongoConnection
 
 object MongoDBSetup {
-val mongoDB = MongoConnection("linus.mongohq.com","10005")("app10536040")
+val mongoDB = MongoConnection("linus.mongohq.com",10005)("app10536040")
 
 
 }

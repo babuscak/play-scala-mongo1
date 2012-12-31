4af8a17866f182a3e76383a19d95c27584233958
Roman Babuscak <roman.babuscak@garwan.sk>
2012-12-31 02:21:29 +0100

    user auth

--- app/models/MongoDBSetup.scala
+++ app/models/MongoDBSetup.scala
@@ -3,6 +3,7 @@ import com.mongodb.casbah.MongoConnection
 
 object MongoDBSetup {
 val mongoDB = MongoConnection("linus.mongohq.com",10005)("app10536040")
-
+if (!mongoDB.authenticate("roman", "roman")) throw new Exception("No authorization in mongoDb")
+        else mongoDB
 
 }
